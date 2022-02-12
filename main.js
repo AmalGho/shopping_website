e05aff219681334ef1665ae05d6fa406af62c46c
Amal <amol43@outlook.com>
2022-02-13 00:39:22 +0300

    add js file amd modify two files

---
+++ main.js
@@ -0,0 +1,24 @@
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    let i;
+    let x = document.getElementsByClassName("content-1");
+
+    if (n > x.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = x.length;
+    } 
+
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "flex";
+}
